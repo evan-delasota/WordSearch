160d7c7d3c8b6771e1419a117c00bb7e7b1ac9a7
evan-delasota <evan_delasota@yahoo.com>
2021-12-25 16:12:56 -0800

    Implemented fillGrid method that fills in words horizontally without overlap, uses wordFitsGrid helper method

--- src/io/etd/App.java
+++ src/io/etd/App.java
@@ -8,15 +8,10 @@ import java.util.concurrent.ThreadLocalRandom;
 public class App {
 
     public static void main(String[] args) {
-        final int gridSize = 10;
-        char[][] grid = new char[gridSize][gridSize];
         List<String> wordList = Arrays.asList("ONE", "TWO", "THREE");
-
-        for (String word : wordList) {
-            int y = ThreadLocalRandom.current().nextInt(0, gridSize);
-            int x = ThreadLocalRandom.current().nextInt(0, gridSize);
-            grid[x][y] = word.charAt(0);
-        }
+        Grid crossword = new Grid(10);
+        crossword.fillGrid(wordList);
+        crossword.displayGrid();
 
 
     }
--- src/io/etd/Grid.java
+++ src/io/etd/Grid.java
@@ -1,25 +1,76 @@
 package io.etd;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.concurrent.ThreadLocalRandom;
+
 public class Grid {
     private int gridSize;
-    private final char[][] grid = new char[gridSize][gridSize];
+    private char[][] grid;
+    private List<Coordinate> coordinates = new ArrayList<>();
+    private class Coordinate {
+        int x;
+        int y;
+
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 
     public Grid(int gridSize) {
         this.gridSize = gridSize;
+        grid = new char[gridSize][gridSize];
+
         for (int i = 0; i < gridSize; ++i) {
             for(int j = 0; j < gridSize; ++j) {
+                coordinates.add(new Coordinate(i, j));
                 grid[i][j] = '_';
             }
         }
     }
+    public void fillGrid(List<String> words) {
+        for (String word : words) {
+            Collections.shuffle(coordinates);
 
-     public void displayGrid() {
-         for (int i = 0; i < gridSize; ++i) {
-             for(int j = 0; j < gridSize; ++j) {
-                 System.out.print(grid[i][j] + " ");
-             }
-             System.out.println();
-         }
-     }
+            for (Coordinate coordinate : coordinates) {
+                int x = coordinate.x;
+                int y = coordinate.y;
+                if (wordFitsGrid(word, coordinate)) {
+                    for (char ch : word.toCharArray()) {
+                        grid[x][y++] = ch;
+                    }
+                    break;
+                }
+            }
+        }
+    }
+
+    private boolean wordFitsGrid(String word, Coordinate coordinate) {
+        int x = coordinate.x;
+        int y = coordinate.y;
+
+        if (y + word.length() < gridSize) {
+            for (int i = 0; i < word.length(); ++i) {
+                if (grid[x][y + i] != '_') {
+                    return false;
+                }
+            }
+            return true;
+        }
+
+        return false;
+    }
+
+
+    public void displayGrid() {
+        for (int i = 0; i < gridSize; ++i) {
+            for(int j = 0; j < gridSize; ++j) {
+                System.out.print(grid[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
 
 }
